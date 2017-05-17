e3027bee62e83dcf1b439a6fbb7e481094d422cb
ChengWiLL <1079029559@qq.com>
2017-05-17 11:17:23 +0800

    bug fixes

--- spider/govcrawl/govcrawl/pipelines.py
+++ spider/govcrawl/govcrawl/pipelines.py
@@ -16,13 +16,15 @@ class GovcrawlPipeline(object):
 		self.seen = set()
 
 	def __getValue(self,url):
+		isHaveManyQueryInUrl = False
 		for value in self.valuedict:
 			div_by_value = url.split(value.rstrip('\n'))
 			mm = div_by_value[0]
 			if mm in self.seen:
 				raise DropItem('Duplicate link %s' % url)
-			elif len(div_by_value) > 1:
+			elif len(div_by_value) > 1 and not isHaveManyQueryInUrl:
 				self.seen.add(mm)
+				isHaveManyQueryInUrl = True
 				line = url+'\n'
 				print url
 				self.file.write(line)
